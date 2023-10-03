47d371ff42359dc22dafe80f621142b0c277734d
equaLize <danyak1945@gmail.com>
2023-10-03 19:14:26 +0300

    Fifth task complete.

---
+++ Task 5_3-9.cpp
@@ -0,0 +1,10 @@
+//#include <iostream>
+//#include <string>
+//
+//int main() {
+//	std::string input;
+//	std::cout << "Print anything: ";
+//	std::getline(std::cin, input);
+//	for (char c : input) std::cout << int(c) << "\n";
+//	return 0;
+//}
