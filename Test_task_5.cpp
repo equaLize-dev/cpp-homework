04a5c9d581afe7a600fc2db64a989ceab23ef3dc
equaLize <danyak1945@gmail.com>
2023-10-09 21:04:45 +0300

    Test

---
+++ Test_task_5.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//
+//int main() {
+//    int N, K;
+//    std::cout << "Print N value: ";
+//    std::cin >> N;
+//    std::cout << "Print K value: ";
+//    std::cin >> K;
+//
+//    while (K != 1) {
+//        N = (N - (K % 2)) / 2;
+//        K = K / 2;
+//    }
+//
+//    std::cout << "\nResult: \n" << (N - 1) / 2 << "\n";
+//    std::cout << N / 2 << "\n";
+//}
